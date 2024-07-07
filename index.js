dd0f0e311d95ba03431e7fa072d567fedb3b9980
Tushar Saxena <90819927+Mrfrost1304@users.noreply.github.com>
2024-07-07 15:18:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+var randomNum1=Math.floor(Math.random()*6)+1;
+
+var randomImg1="images/dice"+randomNum1+".png";
+
+document.querySelectorAll("img")[0].setAttribute("src", randomImg1);
+
+var randomNum2=Math.floor(Math.random()*6)+1;
+
+var randomImg2="images/dice"+randomNum2+".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImg2);
+
+if (randomNum1 > randomNum2) {
+    document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+  }
+  else if (randomNum2 > randomNum1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+  }
+  else {
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
